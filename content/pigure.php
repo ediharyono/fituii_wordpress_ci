0c7742fd7af5857003228e0456c6800b0d9b64a8
Edi Haryono <energipositif@gmail.com>
2023-03-20 05:54:38 +0700

    Create pigure.php

---
+++ content/pigure.php
@@ -0,0 +1,76 @@
+<div id="after_full_slider_1" class="main_color av_default_container_wrap container_wrap fullsize">
+<div class="container av-section-cont-open">
+<div class="template-page content  av-content-full alpha units">
+<div class="post-entry post-entry-type-page">
+<div class="entry-content-wrapper clearfix">
+<div class="flex_column av_one_full avia-builder-el-1 el_after_av_slideshow_full el_before_av_one_full avia-builder-el-first first no_margin flex_column_div av-animated-generic fade-in av-zero-column-padding avia_start_animation avia_start_delayed_animation">
+<section class="avia_codeblock_section  avia_code_block_0" itemscope="itemscope" itemtype="https://schema.org/CreativeWork"><div class="avia_codeblock " itemprop="text"><div class="grid">
+<figure class="effect-sarah">
+<img src="https://kemahasiswaan.uii.ac.id/wp-content/uploads/2023/01/1.png">
+<figcaption>
+<h2><span>Organisasi <br>Kemahasiswaan</span></h2>
+<p>Selengkapnya</p>
+<a href="https://kemahasiswaan.uii.ac.id/informasi/organisasi-kemahasiswaan/">View more</a>
+</figcaption>
+</figure>
+
+<figure class="effect-sarah">
+<img src="https://kemahasiswaan.uii.ac.id/wp-content/uploads/2023/01/2.png">
+<figcaption>
+<h2><span>Prestasi <br> Mahasiswa</span></h2>
+<p>Selengkapnya</p>
+<a href="https://kemahasiswaan.uii.ac.id/informasi/prestasi-karya-mahasiswa/">View more</a>
+</figcaption>
+</figure>
+<figure class="effect-sarah">
+<img src="https://kemahasiswaan.uii.ac.id/wp-content/uploads/2023/01/3-1.png">
+<figcaption>
+<h2><span>Inovasi dan <br>Kewirausahaan</span></h2>
+<p>Selengkapnya</p>
+<a href="https://kemahasiswaan.uii.ac.id/informasi/inovasi-kewirausahaan/">View more</a>
+</figcaption>
+</figure>
+<figure class="effect-sarah">
+<img src="https://kemahasiswaan.uii.ac.id/wp-content/uploads/2023/01/4.png">
+<figcaption>
+<h2><span>Pengabdian Masyarakat <br> &amp; Dakwah Mahasiswa</span></h2>
+<p>Selengkapnya</p>
+<a href="https://kemahasiswaan.uii.ac.id/pengabdian-masyarakat-dakwah-mahasiswa/">View more</a>
+</figcaption>
+</figure>
+</div></div></section></div>
+<div class="flex_column av-3cfvdqb-3eea8321653a23c7f520c6fdab9d9fb6 av_one_full avia-builder-el-3 el_after_av_one_full el_before_av_section avia-builder-el-last first no_margin flex_column_div av-animated-generic fade-in av-zero-column-padding column-top-margin avia_start_animation avia_start_delayed_animation"><section class="avia_codeblock_section  avia_code_block_1" itemscope="itemscope" itemtype="https://schema.org/CreativeWork"><div class="avia_codeblock " itemprop="text"><div class="grid">
+<figure class="effect-sarah">
+<img src="https://kemahasiswaan.uii.ac.id/wp-content/uploads/2023/01/5.png">
+<figcaption>
+<h2><span>Informasi<br> Beasiswa</span></h2>
+<p>Selengkapnya.</p>
+<a href="https://kemahasiswaan.uii.ac.id/informasi/beasiswa/">View more</a>
+</figcaption>
+</figure>
+<figure class="effect-sarah">
+<img src="https://kemahasiswaan.uii.ac.id/wp-content/uploads/2023/01/6-1.png">
+<figcaption>
+<h2><span>Layanan <br>Konseling<span></span></span></h2>
+<p>Selengkapnya</p>
+<a href="https://kemahasiswaan.uii.ac.id/layanan/konseling-mahasiswa/">View more</a>
+</figcaption>
+</figure>
+<figure class="effect-sarah">
+<img src="https://kemahasiswaan.uii.ac.id/wp-content/uploads/2023/01/7.png">
+<figcaption>
+<h2><span>Reputasi<br> Internasional</span></h2>
+<p>Selengkapnya.</p>
+<a href="https://kemahasiswaan.uii.ac.id/reputasi-internasional/">View more</a>
+</figcaption>
+</figure>
+<figure class="effect-sarah">
+<img src="https://kemahasiswaan.uii.ac.id/wp-content/uploads/2023/01/8.png">
+<figcaption>
+<h2><span>Agenda<br> Kemahasiswaan</span></h2>
+<p>Selengkapnya</p>
+<a href="https://kemahasiswaan.uii.ac.id/informasi/agenda-kemahasiswaan/">View more</a>
+</figcaption>
+</figure>
+</div></div></section></div>
+</div></div></div></div></div>
